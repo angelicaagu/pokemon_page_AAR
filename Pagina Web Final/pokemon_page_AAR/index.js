406623edf2d0acda593f871ed612f48364af5372
Angelica Agudelo <aagudelo483@soyudemedellin.edu.co>
2023-11-09 11:55:39 -0500

    sexto guardado

--- Pagina Web Final/pokemon_page_AAR/index.js
+++ Pagina Web Final/pokemon_page_AAR/index.js
@@ -5,55 +5,59 @@ console.log(data.pokemon)
 
 const pokemonHtml = data.pokemon.map((element) => {
   return `
-  <article class="pokemon-item">
-  <div class="image-wrapper">
-    <img src="${element.Images[0]}">
-  </div>
-  <div class="content">
-    <h4>${element.Title}</h4>
-    <div class="poke-info">
-      <div class="poke-num">
-        <p>${element.imdbRating}/10</p>
-      </div>
-      <span class="type">${element.Genre}</span>
+  <section class="pokemon-container">
+    <article class="pokemon-item">
+    <div class="image-wrapper">
+      <img src="${element.image}">
     </div>
-    <p class="description">${element.Plot}</p>
-  </div>
-</article>
+    <div class="content">
+      <h4>${element.pokemon}</h4>
+      <div class="poke-info">
+      <div class="poke-num">
+        <p>${element["pokedex-num"]}?}/10</p>
+        </div>
+        <span class="type">${element.type}</span>
+      </div>
+      <p class="description">${element.description}</p>
+    </div>
+  </article>
+</section>
   `
 }).join("")
 
-document.getElementsByClassName("pokemon-container")[0].innerHTML = filmsHtml
+document.getElementsByClassName("pokemon-container")[0].innerHTML = pokemonHtml
 
 document.querySelectorAll(".filter-button").forEach((buttonElement) => {
   buttonElement.addEventListener("click", (e) => {
     console.log(e.target.textContent);
     const selectedType = e.target.textContent;
     const newPokemon = data.pokemon.filter(
-      (element) => element.Genre.includes(selectedType)
+      (element) => element.type.includes(selectedType)
     );
     console.log(newPokemon)
 
-    const newpokemonHtml = newPokemon.map((element) => {
+    const newPokemonHtml = newPokemon.map((element) => {
       return `
-      <article class="pokemon-item">
-      <div class="image-wrapper">
-        <img src="${element.Images[0]}">
-      </div>
-      <div class="content">
-        <h4>${element.Title}</h4>
-        <div class="poke-info">
-          <div class="poke-num">
-            <p>${element.imdbRating}/10</p>
-          </div>
-          <span class="type">${element.Genre}</span>
+    <section class="pokemon-container">
+        <article class="pokemon-item">
+        <div class="image-wrapper">
+         <img src="${element.image}">
         </div>
-        <p class="description">${element.Plot}</p>
-      </div>
-    </article>
+        <div class="content">
+          <h4>${element.pokemon}</h4>
+          <div class="poke-info">
+            <div class="poke-num">
+              <p>${element["pokedex-num"]}</p>
+            </div>
+            <span class="type">${element.type}</span>
+          </div>
+          <p class="description">${element.description}</p>
+        </div>
+      </article>
+    </section>
       `
     }).join("")
 
-    document.getElementsByClassName("pokemon-container")[0].innerHTML = newpokemonHtml
+    document.getElementsByClassName("pokemon-container")[0].innerHTML = newPokemonHtml
   });
 });
